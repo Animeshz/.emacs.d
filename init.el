a5fbfdef3713b39ab4663165064554077c2a8256
Animesh Sahu <animeshsahu19@yahoo.com>
2023-04-01 22:56:33 +0530

    Add corfu + tempel, add graphviz, dont enable parinfer for every org file, finish up all todos

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; References: https://www.reddit.com/r/emacs/comments/v62ef0/parallel_package_installation <=> https://github.com/progfolio/elpaca
 
-(defvar elpaca-installer-version 0.2)
+(defvar elpaca-installer-version 0.3)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
